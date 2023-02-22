169057c4a78de8a2fd45e031d46328135862d0a6
Seif2Balea <117376889+Seif2Balea@users.noreply.github.com>
2023-02-22 14:52:49 +0100

    2nd commit

--- kata.py
+++ kata.py
@@ -11,15 +11,29 @@ class FizzBuzz:
         if inputNumber % 3 == 0:
             return 'Fizz'
 
-        return '0'
+        if inputNumber % 5 == 0:
+            return 'buzz'
+
+        return str(inputNumber)
 
 
 class TestKata:
 
+    @pytest.mark.parametrize("test_input,expected", [(0, '0'), (1, '1'), (2,'2')])
+    def test_eval(self,test_input, expected):
+        assert FizzBuzz.compute(test_input) == expected
+
     def test_0(self):
+
         assert FizzBuzz.compute(0) == '0'
 
     def test_fizz(self):
         assert FizzBuzz.compute(3) == 'Fizz'
+
+    def test_fizz_1(self):
+        assert FizzBuzz.compute(1) == '1'
+
+    def test_buzz(self):
+        assert FizzBuzz.compute(5) == 'buzz'
 
 

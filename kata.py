d8c40bdbaa63391fc5f9e37394d4a9d340be66fc
joann.jousselin <joann.jousselin@gmail.com>
2023-02-22 14:37:45 +0100

    Test 1

--- kata.py
+++ kata.py
@@ -1,11 +1,25 @@
 import pytest
 
 
+class FizzBuzz:
+
+    @staticmethod
+    def compute(inputNumber: int):
+        if inputNumber == 0:
+            return '0'
+
+        if inputNumber % 3 == 0:
+            return 'Fizz'
+
+        return '0'
+
+
 class TestKata:
 
-    def test_todo(self):
-        assert '0' == '0'
-        print('test_github')
-        
+    def test_0(self):
+        assert FizzBuzz.compute(0) == '0'
 
-     
+    def test_fizz(self):
+        assert FizzBuzz.compute(3) == 'Fizz'
+
+
